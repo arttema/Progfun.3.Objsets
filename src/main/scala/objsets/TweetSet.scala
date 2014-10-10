815246ed3122002557465f7dcedc1f8a487c7d34
Artem Stolpovski <ArtemStolpovski@coherentsolutions.com>
2014-10-10 15:15:42 +0300

    Most Rt initially implemented with hack

--- src/main/scala/objsets/TweetSet.scala
+++ src/main/scala/objsets/TweetSet.scala
@@ -34,12 +34,15 @@ class Tweet(val user: String, val text: String, val retweets: Int) {
  * [1] http://en.wikipedia.org/wiki/Binary_search_tree
  */
 abstract class TweetSet {
+  val any: Tweet
 
   /**
    * This method takes a predicate and returns a subset of all the elements
    * in the original set for which the predicate is true.
    */
-  def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, new Empty)
+  def filter(p: Tweet => Boolean): TweetSet = {
+    filterAcc(p, new Empty)
+  }
 
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -53,9 +56,6 @@ abstract class TweetSet {
 
   /**
    * Returns the tweet from this set which has the greatest retweet count.
-   *
-   * Question: Should we implment this method here, or should it remain abstract
-   * and be implemented in the subclasses?
    */
   def mostRetweeted: Tweet
 
@@ -65,10 +65,8 @@ abstract class TweetSet {
    * have the highest retweet count.
    *
    * Hint: the method `remove` on TweetSet will be very useful.
-   * Question: Should we implment this method here, or should it remain abstract
-   * and be implemented in the subclasses?
    */
-  def descendingByRetweet: TweetList = ???
+  def descendingByRetweet: TweetList
 
   /**
    * Returns a new `TweetSet` which contains all elements of this set, and the
@@ -92,6 +90,8 @@ abstract class TweetSet {
    * This method takes a function and applies it to every element in the set.
    */
   def foreach(f: Tweet => Unit): Unit
+
+  def isEmpty: Boolean
 }
 
 class Empty extends TweetSet {
@@ -108,8 +108,15 @@ class Empty extends TweetSet {
 
   def mostRetweeted = throw new NoSuchElementException("List Is Empty!")
 
+  def descendingByRetweet: TweetList = Nil
+
   def union(that: TweetSet): TweetSet = that
+
+  def isEmpty = true
+
+  val any: Tweet = new Tweet("","",-1)
 }
+
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
@@ -143,14 +150,51 @@ class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
     right.foreach(f)
   }
 
-  def mostRetweeted: Tweet = ???
+  /*
+  todo The more often a tweet is “re-tweeted” (that is, repeated by a different user with or without
+   additions), the more influential it is.
 
-  /**
-   * Returns a new `TweetSet` that is the union of `TweetSet`s `this` and `that`.
-   *
-   * Question: Should we implment this method here, or should it remain abstract
-   * and be implemented in the subclasses?
+The goal of this part of the exercise is to add a method descendingByRetweet to TweetSet which
+ should produce a linear sequence of tweets (as an instance of class TweetList), ordered by their
+ number of retweets:
+
+def descendingByRetweet: TweetList
+
+  This method reflects a common pattern when transforming data structures. While traversing one data
+  structure (in this case, a TweetSet), we’re building a second data structure (here, an instance of
+  class TweetList). The idea is to start with the empty list Nil (containing no tweets), and to find
+   the tweet with the most retweets in the input TweetSet. This tweet is removed from the TweetSet
+  (that is, we obtain a new TweetSet that has all the tweets of the original set except for the
+   tweet that was “removed”; this immutable set operation, remove, is already implemented for you),
+   and added to the result list by creating a new Cons. After that, the process repeats itself, but now
+   we are searching through a TweetSet with one less tweet.
+
+Hint: start by implementing the method mostRetweeted which returns the most popular tweet of a TweetSet.
    */
+  def mostRetweeted: Tweet = {
+
+    def mostRetweetedHelper(current: Tweet, acc: TweetSet): Tweet = {
+      if(acc.isEmpty) return current
+      val max = if (current.retweets < acc.any.retweets) elem else current
+      mostRetweetedHelper(max, acc.remove(acc.any))
+    }
+
+    mostRetweetedHelper(elem,this)
+  }
+
+  //  def descendingByRetweet: TweetList = {
+  //     var res: TweetList = Nil
+  //     var list: TweetSet = this
+  //
+  //    while (list isInstanceOf NonEmpty){
+  //
+  //       val next = mostRetweeted
+  //       res = new Cons(mostRetweeted, res)
+  //       list = remove(mostRetweeted)
+  //     }
+  //     return res
+  //  }
+
   def union(that: TweetSet): TweetSet = {
     var res = that
     if(!that.contains(elem)){
@@ -160,6 +204,19 @@ class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
     val r = right.union(l)
     r
   }
+
+  def isEmpty = false
+
+  /**
+   * Returns a list containing all tweets of this set, sorted by retweet count
+   * in descending order. In other words, the head of the resulting list should
+   * have the highest retweet count.
+   *
+   * Hint: the method `remove` on TweetSet will be very useful.
+   */
+  override def descendingByRetweet: TweetList = ???
+
+  val any: Tweet = elem
 }
 
 trait TweetList {
@@ -187,7 +244,35 @@ class Cons(val head: Tweet, val tail: TweetList) extends TweetList {
 object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
+  /*
+  todo 4 Tying everything together
 
+In the last step of this assignment your task is to detect influential tweets in a set of recent
+tweets. We are providing you with a TweetSet containing several hundred tweets from popular tech
+ news sites in the past few days, located in the TweetReader object (file “TweetReader.scala”).
+ TweetReader.allTweets returns an instance of TweetSet containing a set of all available tweets.
+
+Furthermore, you are given two lists of keywords. The first list corresponds to keywords associated
+ with Google and Android smartphones, while the second list corresponds to keywords associated
+  with Apple and iOS devices. Your objective is to detect which platform has generated more
+  interest or activity in the past few days.
+
+As a first step, use the functionality you implemented in the first parts of this assignment
+ to create two different TweetSets, googleTweets and appleTweets. The first TweetSet,
+  googleTweets, should contain all tweets that mention (in their “text”) one of the
+  keywords in the google list. The second TweetSet, appleTweets, should contain all
+  tweets that mention one of the keyword in the apple list. Their signature is as follows:
+
+lazy val googleTweets: TweetSet
+lazy val appleTweets: TweetSet
+
+Hint: use the exists method of List and contains method of class java.lang.String.
+
+From the union of those two TweetSets, produce trending, an instance of class TweetList
+ representing a sequence of tweets ordered by their number of retweets:
+
+lazy val trending: TweetList
+   */
   lazy val googleTweets: TweetSet = ???
   lazy val appleTweets: TweetSet = ???
 
